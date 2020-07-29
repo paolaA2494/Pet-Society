653f82848422b0d8df9df5444fd220159f8af686
Casta <palolahrn0104@gmail.com>
2020-07-29 13:58:01 -0500

    Se cambio el proyecto en react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <table >
+          <thead>
+            <tr>
+              <th>Nombres</th>
+              <th>Correo</th>
+              <th>Cedula</th>
+              <th>Telefono</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>paola hernandez</td>
+              <td>cmtecnica09@elpoli.edu.co</td>
+              <td>1033300000000</td>
+              <td>3043953342</td> 
+            </tr>
+            <tr>
+              <td>1,002</td>
+              <td>amet</td>
+              <td>consectetur</td>
+              <td>adipiscing</td>
+              <td>elit</td>
+            </tr>
+            <tr>
+              <td>1,003</td>
+              <td>Integer</td>
+              <td>nec</td>
+              <td>odio</td>
+              <td>Praesent</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
